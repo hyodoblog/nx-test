6a32e7613bd8cd5138f19356d915813fb9144bea
hyodoblog <yusei.hyodo@gmail.com>
2022-05-24 16:24:56 +0900

    port 設定

--- apps/admin/pages/index.tsx
+++ apps/admin/pages/index.tsx
@@ -17,8 +17,7 @@ export function Index() {
         <div className="container">
           <div id="welcome">
             <h1>
-              <span> Hello there, </span>
-              Welcome admin 👋
+              <span> Hello, </span>
             </h1>
           </div>
 
